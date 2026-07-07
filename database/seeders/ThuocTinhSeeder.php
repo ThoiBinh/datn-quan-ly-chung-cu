2da358fbe0f89c8f79868b1df013cc70881f9e61
Manhcuong-cmd <truongmanhcuong2017@gmail.com>
2026-07-07 17:09:09 +0700

    fix confilic

--- database/seeders/ThuocTinhSeeder.php
+++ database/seeders/ThuocTinhSeeder.php
@@ -19,14 +19,10 @@ class ThuocTinhSeeder extends Seeder
 
         foreach ($data as $item) {
             if (!DB::table('thuoc_tinh')->where('ten_thuoc_tinh', $item['ten_thuoc_tinh'])->exists()) {
-<<<<<<< HEAD
                 DB::table('thuoc_tinh')->insert(array_merge($item, [
                     'createdAt' => now(),
                     'updatedAt' => now(),
                 ]));
-=======
-                DB::table('thuoc_tinh')->insert($item);
->>>>>>> origin/develop
             }
         }
     }
